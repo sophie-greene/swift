5e498c1aedd38e6167a77f2309a105398801ad6c
Sophie Greene <sophiegreene@Sophies-MBP.lan>
2017-01-30 15:39:25 +0000

    init cmt

---
+++ developer/sophieCalc/sophieCalc/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  sophieCalc
+//
+//  Created by Sophie Greene on 29/01/2017.
+//  Copyright © 2017 Sophie Greene. All rights reserved.
+
+//all user interface module
+import UIKit
+//child: parent inheritance 
+class ViewController: UIViewController {
+    var userIsIntheMiddleOfTyping = false
+    
+    @IBOutlet weak var display: UILabel!
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsIntheMiddleOfTyping{
+            let currentlyDisplayed = display.text!
+            display.text = currentlyDisplayed + digit
+        }else{
+            display.text = digit
+        }
+        userIsIntheMiddleOfTyping = true
+    }
+
+   
+    @IBAction func mathOperation(_ sender: UIButton) {
+        userIsIntheMiddleOfTyping  = false
+        if let mathSymbol = sender.currentTitle{
+            if mathSymbol == "π"{
+                display.text = String(M_PI)
+            }
+        }
+    }
+
+
+}
+
